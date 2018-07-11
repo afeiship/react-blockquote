f5d78e20dbd6bb44d653f7acff738be7913e03f8
afeiship <1290657123@qq.com>
2018-07-11 15:21:47 +0800

    issue: update pkg

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -15,7 +15,6 @@ var externals = process.env.NODE_ENV === 'production' ? {
   'classnames': 'classnames',
   'react-dom': 'react-dom',
   'noop': 'noop',
-  'mixin-decorator': 'mixin-decorator',
   'object-assign': 'object-assign',
   'prop-types': 'prop-types',
 } : {};
